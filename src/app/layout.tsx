e968baf4e380f7cf8f0f144cb7bc71f7205d442f
Balsamic Vinigga <Munelkop.Khuzwayo@gmail.com>
2025-07-10 10:21:44 +0000

    aded line and changed title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import './globals.css';
 import { Toaster } from "@/components/ui/toaster"
 
 export const metadata: Metadata = {
-  title: 'Echoes of Karin',
+  title: 'Karin Kedem Music',
   description: 'Karin Kedem, Art, Life, and Music',
 };
 
